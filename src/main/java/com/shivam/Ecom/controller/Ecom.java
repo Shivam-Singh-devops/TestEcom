759ff44fbb1b43d0a715c91b2f2da83a96992dc4
shivam <lordshiva8894@gmail.com>
2026-02-11 15:34:25 +0530

    fix /

--- src/main/java/com/shivam/Ecom/controller/Ecom.java
+++ src/main/java/com/shivam/Ecom/controller/Ecom.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("app")
+@RequestMapping("/app")
 public class Ecom {
 
     @Autowired
@@ -17,30 +17,30 @@ public class Ecom {
     // crud operations
 
     // read operation
-    @GetMapping("getAll")
+    @GetMapping("/getAll")
     public List<itemModel> getAll(){
         return service.getAll();
     }
 
     // get single element
-    @GetMapping("get/{id}")
+    @GetMapping("/get/{id}")
     public itemModel getByID(@PathVariable("id") int id){
         return service.getByID(id);
     }
 
     // create operation
-    @PostMapping("addItem")
+    @PostMapping("/addItem")
     public itemModel addItem(@RequestBody itemModel i){
          return service.addItem(i);
     }
 
     // delete operation
-    @DeleteMapping("delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteItem(@PathVariable("id") int id){
         service.deleteItem(id);
     }
 
-    @PutMapping("update/{id}")
+    @PutMapping("/update/{id}")
     public itemModel updateItem(
             @PathVariable int id,
             @RequestBody itemModel updatedItem
